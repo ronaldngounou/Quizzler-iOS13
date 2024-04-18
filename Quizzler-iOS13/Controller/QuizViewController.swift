fd036e56d3e5bfc45a97c9eec25e74a87d65ecd3
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2024-04-18 11:57:45 -0400

    Attempt to connect the new QuizViewController and its outlets
    - TO DO: Update labels and their connections to the view
    - 3 buttons connected to the same function.

---
+++ Quizzler-iOS13/Controller/QuizViewController.swift
@@ -0,0 +1,21 @@
+//
+//  QuizViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Ronald Ngounou on 4/18/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController : UIViewController{
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    
+}
---
+++ Quizzler-iOS13/Controller/ViewController2.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController2.swift
+//  Quizzler-iOS13
+//
+//  Created by Ronald Ngounou on 4/18/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
